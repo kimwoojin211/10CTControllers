6347a718723095870fbd641687d8d8e705446de8
Woo Jin Kim <kimwoojin211@gmail.com>
2022-05-11 16:41:06 +0900

    finalize home page formatting

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,26 +4,19 @@ import 'react-slideshow-image/dist/styles.css';
 import sliderImg1 from '../img/homeBackground/gcc1.jpg';
 import sliderImg2 from '../img/homeBackground/gcc2.jpg';
 import sliderImg3 from '../img/homeBackground/gcc3.jpg';
-import sliderImg4 from '../img/homeBackground/desk1.jpg';
-import sliderImg5 from '../img/homeBackground/gcc4.jpg';
-import Suf from '../img/players/Suf.jpg';
-import Asashi from '../img/players/Asashi.jpg';
-import Casper from '../img/players/Casper.jpg';
-import Squid from '../img/players/Squid.jpg';
-import Venelox from '../img/players/Venelox.jpg';
-import Franz from '../img/players/Franz.jpg';
+import sliderImg4 from '../img/homeBackground/paracords.jpg';
+import sliderImg5 from '../img/homeBackground/desk1.jpg';
 
 const sliderImages= [
-  { url: sliderImg1 },
-  { url: sliderImg2 },
-  { url: sliderImg3 },
-  { url: sliderImg4 },
-  { url: sliderImg5 },
+  { imgURL: sliderImg1 },
+  { imgURL: sliderImg2 },
+  { imgURL: sliderImg3 },
+  { imgURL: sliderImg4 },
+  { imgURL: sliderImg5 },
 ];
 
 function Hero(){
   return(
-    <React.Fragment>
       <div className="hero">
         <Fade
           duration={4000}
@@ -32,54 +25,14 @@ function Hero(){
           canSwipe={false}
         >
           {
-          sliderImages.map((fadeImage, index) => (
-            <div className="each-fade" key={index}>
-              <div className="image-container">
-                <img className="sliderBackground" src={fadeImage.url} />
-                <div className="heroContent overlay">
-                  {/* <div className="heroText container">
-                    <h2 className="heroTitle">10CT Controllers</h2>
-                    <p className="heroSubtitle"><i>SoCal Controller Modder</i></p>
-                  </div> */}
-                  {/* <div className="pastClients container">
-                    <p>Affordable, tournament-grade repairs & custom-built controllers for players like:</p>
-                    <div className="players row">
-                      <div className="featuredPlayers row">
-                        <div className="player">
-                          <img className="playerPic img--small" src={Squid} alt="Squid"></img>
-                          <p className="playerTag">Squid</p>
-                        </div>
-                        <div className="player">
-                          <img className="playerPic img--small" src={Suf} alt="Suf"></img>
-                          <p className="playerTag">Suf</p>
-                        </div>
-                        <div className="player">
-                          <img className="playerPic img--small" src={Asashi} alt="Asashi"/>
-                          <p className="playerTag">Asashi</p>
-                        </div>
-                        <div className="player">
-                          <img className="playerPic img--small" src={Casper} alt="Casper"></img>
-                          <p className="playerTag">Casper</p>
-                        </div>
-                        
-                        <div className="player">
-                          <img className="playerPic img--small" src={Venelox} alt="Venelox"></img>
-                          <p className="playerTag">Venelox</p>
-                        </div>
-                        <div className="player">
-                          <img className="playerPic img--small" src={Franz} alt="Franz"></img>
-                          <p className="playerTag">Franz</p>
-                        </div>  
-                      </div>
-                      <div className="andMore">
-                        <h4>& more!</h4>
-                      </div>
-                    </div>
-                  </div> */}
+            sliderImages.map((fadeImage, index) => (
+              <div className="each-fade" key={index}>
+                <div className="image-container">
+                  <img className="sliderBackground" src={fadeImage.imgURL} />
+                  <div className="overlay"/>
                 </div>
               </div>
-            </div>
-          ))
+            ))
           }
         </Fade>
         <div className="heroText container">
@@ -87,7 +40,6 @@ function Hero(){
           <p className="heroSubtitle"><i>SoCal Controller Modder</i></p>
         </div>
     </div>
-    </React.Fragment>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,67 +13,51 @@ import Squid from '../img/players/Squid.jpg';
 import Venelox from '../img/players/Venelox.jpg';
 import Franz from '../img/players/Franz.jpg';
 
+const playerArray = [{"Squid":Squid}, {"Suf":Suf}, {"Asashi":Asashi}, {"Casper":Casper}, {"Venelox":Venelox}, {"Franz":Franz}]
 const testimonialArray = ["1472063565095915523", "1448828944652795906", "1402415528673697792", "1420643017522774018", "1427355646794555393", "1430298525250715650", "1430996181438681095", "1441509134789222400", "1442394120656355334"]
 
 function Home(){
+  console.log(playerArray);
   return(
     <React.Fragment>
       <Hero/>
-      <div className="pastClients">
-        <div class="container">
-          <h2 className="contentTitle">Affordable, tournament-grade repairs & custom-built controllers for players such as:</h2>
-          <div className="players row">
-            <div className="featuredPlayers row">
-              <div className="player">
-                <img className="playerPic img--med" src={Squid} alt="Squid"></img>
-                <p className="playerTag">Squid</p>
-              </div>
-              <div className="player">
-                <img className="playerPic img--med" src={Suf} alt="Suf"></img>
-                <p className="playerTag">Suf</p>
-              </div>
-              <div className="player">
-                <img className="playerPic img--med" src={Asashi} alt="Asashi"/>
-                <p className="playerTag">Asashi</p>
-              </div>
-              <div className="player">
-                <img className="playerPic img--med" src={Casper} alt="Casper"></img>
-                <p className="playerTag">Casper</p>
-              </div>
-          
-              <div className="player">
-                <img className="playerPic img--med" src={Venelox} alt="Venelox"></img>
-                <p className="playerTag">Venelox</p>
-              </div>
-              <div className="player">
-                <img className="playerPic img--med" src={Franz} alt="Franz"></img>
-                <p className="playerTag">Franz</p>
-              </div>
+      <div className="pastClientsContainer">
+        <div className="container col">
+          <h2 className="contentTitle">Providing affordable, tournament-grade controllers & repairs for players such as:</h2>
+          <div className="pastClients col">
+            <div className="playerList row">
+              {
+                playerArray.map((player,index)=>(
+                  <div className="player">
+                    <img className="playerPic img--med" src={Object.values(player)[0]} alt={Object.keys(player)[0]}/>
+                    <h5 className="playerTag">{Object.keys(player)[0]}</h5>
+                  </div>
+                ))
+              }
             </div>
+            <h5 className="andMore">& many more!</h5>
           </div>
-          <h5 className="andMore">& many more!</h5>
         </div>
       </div>
-      <div className="CTA">
-        <div className="container">
-          <div className="CTAContent" >
-            <h2 className="CTATitle">Already know what mods or repairs you want for your controller?</h2>
+      <div className="CTA col">
+          <div className="CTAContent container col">
+            <h3 className="CTATitle">Already know what mods or repairs you want for your controller?</h3>
             <a className="CTACommission button" href="https://docs.google.com/forms/d/e/1FAIpQLSetB5_ocLehaWCkuh77VvXX6bjMfFWCPrtmXZMEwcSN1OIskg/viewform">Click here to get started on a commission form.</a>
           </div>
-          <div className="CTAContent" >
-            <h2 className="CTATitle">Not sure which modifications to get for your controller?</h2>
-            <Link to="/ModList" className="CTAModList button">Click here for a list of available mods and their prices.</Link>
-            <h4>or</h4>
-            <Link to="/Premades" className="CTAPremades button">Click here for stocked premade modded controllers, ready for order and delivery.</Link>
+          <div className="CTAContent container col" >
+            <h3 className="CTATitle">Not sure which mods to get for your controller?</h3>
+            <div className="CTAOptions col">
+              <Link to="/ModList" className="CTAModList button">Click here for a full list of mods and their prices.</Link>
+              <Link to="/Premades" className="CTAPremades button">Click here for a list of premade modded controllers, available for order.  </Link>
+            </div>
           </div>
-          <div className="CTAContent" >
-            <h2 className="CTATitle">Questions?</h2>
+          <div className="CTAContent container col">
+            <h3 className="CTATitle">Questions?</h3>
             <ul>
-              <li>Check out the <Link to="/FAQ" className="CTAFAQ">FAQ</Link> page for common modding questions and concerns.</li>
-              <li>DM me on <a className="CTATwitter" href="https://twitter.com/in10cityGCC">Twitter</a>, and make sure to follow for updates on new premade controller availability.</li>
+              <li>Check out the <Link to="/FAQ" className="CTAFAQ">FAQ</Link> page for common modding questions.</li>
               <li>Join the <a className="CTADiscord" href="https://discord.gg/EajRdDrR">10CT  Discord</a> and check the channels to see if your question's already been answered!</li>
+              <li>DM me on <a className="CTATwitter" href="https://twitter.com/in10cityGCC">Twitter</a>, and make sure to follow for updates on new premade controller availability.</li>
             </ul>
-          </div>
         </div>
       </div>
       <div className="testimonials">
@@ -105,17 +89,6 @@ function Home(){
           </div> */}
         </div>
       </div>
-{/* 
-        <div style={{width:500,border:'2px solid black'}}>
-          <TwitterTimelineEmbed
-            sourceType="profile"
-            screenName="in10cityGCC"
-            theme="dark"
-            noFooter="true"
-            transparent="true"
-            options={{height:400}}
-          />
-        </div> */}
     </React.Fragment>
   )
 }
