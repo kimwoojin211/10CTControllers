8bb21cb396fd57077e60d09766d19d03ed5d6bac
Woo Jin Kim <300hHZ@gmail.com>
2022-07-14 22:20:58 -0700

    Updates

---
+++ src/components/PremadeController.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import premade1 from '../img/premades/premade1.jpg';
+
+function PremadeController(props){
+  return(
+    <div className="premadeControllerContainer">
+      <div className="premadeImgWrapper">
+        <img className="premadeImg" src={props.premadeImgURL} alt="Premade Controller 1"/>
+      </div>
+      <div className="premadeInfoContainer">
+        <h2>Specs:</h2>
+        <div className="premadeSpecsWrapper">
+          <ul className="premadeSpecs">
+            {
+              props.premadeSpecList.map((spec,index) =>
+                <li><p>{spec}</p></li>
+              )
+            }
+            {/* <li>Indigo Top / JP White Bottom</li>
+            <li>Brand New T3 board from an boxed Ult edition</li>
+            <li>Wavedash Notches + Vanilla Shield Drop</li>
+            <li>No Reset + Adjustable Snapback Module</li> */}
+          </ul>
+        </div>
+        <div className="premadePrice">
+          <h2 className="">${props.premadePrice}</h2>
+          {/* <button className="premadeButton">Click to DM about this controller</button> */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PremadeController;
---
+++ src/pages/Admin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Admin(){
+  return(
+    <React.Fragment>
+      <div className="admin-container">
+        
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Admin;
